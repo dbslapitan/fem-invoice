aad603d85a2ff7cb047d0400ac400b1b71e3e2db
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-15 05:15:12 -0400

    Add check system for dark-mode.

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,22 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+const isSystemDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+let darkMode = localStorage.getItem('isDarkMode');
+const body = document.body;
+if(!darkMode){
+  localStorage.setItem('isDarkMode', isSystemDarkMode.toString());
+  darkMode = isSystemDarkMode.toString();
+}
+
+const isDarkMode = darkMode === 'true';
+
+if(isDarkMode && !body.classList.contains('dark-mode')){
+  body.classList.add('dark-mode');
+}
+else {
+  if(body.classList.contains('dark-mode')){
+    body.classList.remove('dark-mode');
+  }
+}
