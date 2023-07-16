22708760f6c20a3d2dc4ab6f6df59ecb1f18d1d3
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-16 03:31:39 -0400

    Toggle Dark Mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'invoices';
+export class AppComponent implements OnInit{
+
+  isDarkMode: boolean = false;
+
+  ngOnInit() {
+    this.isDarkMode = localStorage.getItem('isDarkMode') === 'true';
+  }
+
+  changeTheme(){
+    this.isDarkMode = !this.isDarkMode;
+    localStorage.setItem('isDarkMode', this.isDarkMode.toString());
+    document.body.classList.toggle('dark-mode');
+  }
 }
