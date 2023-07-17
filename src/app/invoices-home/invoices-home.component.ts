1e0f98fe082135659067082f691c5ccfc0dc911d
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-17 05:16:25 -0400

    Fixed display string for invoice count when changed to mobile

--- src/app/invoices-home/invoices-home.component.ts
+++ src/app/invoices-home/invoices-home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {map, Observable} from "rxjs";
 
 @Component({
   selector: 'invoices-home',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
     './invoices-home-tablet.component.css',
     './invoices-home-desktop.component.css']
 })
-export class InvoicesHomeComponent {
+export class InvoicesHomeComponent implements OnInit{
 
+  isNotMobile$!: Observable<boolean>;
+
+  constructor(private breakPoint: BreakpointObserver) {
+  }
+
+  ngOnInit() {
+    this.isNotMobile$ = this.breakPoint.observe('(min-width: 768px)').pipe(
+      map(({ matches }) => matches)
+    );
+  }
 }
