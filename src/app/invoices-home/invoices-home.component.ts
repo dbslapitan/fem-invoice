131d3b61d6501eadf96725e77f4d7e9a84afbea8
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-17 04:44:04 -0400

    Fixed title container font size of screen change

--- src/app/invoices-home/invoices-home.component.ts
+++ src/app/invoices-home/invoices-home.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'invoices-home',
   templateUrl: './invoices-home.component.html',
-  styleUrls: ['./invoices-home.component.css']
+  styleUrls: ['./invoices-home.component.css',
+    './invoices-home-tablet.component.css',
+    './invoices-home-desktop.component.css']
 })
 export class InvoicesHomeComponent {
 
