d50267f1853da58a6427c55ea50941062e44f0fa
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-25 22:25:13 -0400

    Complete desktop invoice-list

--- src/app/invoices-home/invoice-list/invoice-list.component.ts
+++ src/app/invoices-home/invoice-list/invoice-list.component.ts
@@ -5,7 +5,8 @@ import {Invoice} from "../../models/invoice.model";
   selector: 'invoice-list',
   templateUrl: './invoice-list.component.html',
   styleUrls: ['./invoice-list.component.css',
-    './invoice-list-tablet.component.css']
+    './invoice-list-tablet.component.css',
+    './invoice-list-desktop.component.css']
 })
 export class InvoiceListComponent {
 
