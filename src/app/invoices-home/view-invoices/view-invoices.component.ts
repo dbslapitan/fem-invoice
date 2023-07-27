138e9bf9fd33d2673f0ee9ea153836b59127f506
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-27 14:51:10 -0400

    Status container finish

--- src/app/invoices-home/view-invoices/view-invoices.component.ts
+++ src/app/invoices-home/view-invoices/view-invoices.component.ts
@@ -3,6 +3,9 @@ import {InvoiceService} from "../../services/invoice.service";
 import {ActivatedRoute} from "@angular/router";
 import {FullInvoice} from "../../models/full-invoice";
 import {map, Observable} from "rxjs";
+import {Invoice} from "../../models/invoice.model";
+import {Address} from "../../models/address.model";
+import {Item} from "../../models/item.model";
 
 @Component({
   selector: 'view-invoices',
@@ -11,15 +14,17 @@ import {map, Observable} from "rxjs";
 })
 export class ViewInvoicesComponent implements OnInit{
 
-  fullInvoice$!: Observable<FullInvoice>;
+  invoice!: Invoice;
+  addresses!: Address[];
+  items!: Item[];
 
   constructor(private invoiceService: InvoiceService, private activatedRoute: ActivatedRoute) {
   }
 
   ngOnInit() {
-    const stringId = this.activatedRoute.snapshot.data['stringId'];
-    this.fullInvoice$ = this.activatedRoute.data.pipe(
-      map(data => data['fullInvoice'])
-    );
+    const fullInvoice = this.activatedRoute.snapshot.data['fullInvoice'];
+    this.invoice = fullInvoice.invoice;
+    this.addresses = fullInvoice.addresses;
+    this.items = fullInvoice.items;
   }
 }
