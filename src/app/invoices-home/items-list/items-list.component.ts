f7167e992c0ec2755c5b868d1de4b27a2863305e
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-07-30 04:35:34 -0400

    Display Item details on tablet

--- src/app/invoices-home/items-list/items-list.component.ts
+++ src/app/invoices-home/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Item} from "../../models/item.model";
 
 @Component({
   selector: 'items-list',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class ItemsListComponent {
 
+  @Input()
+  items!: Item[];
 }
