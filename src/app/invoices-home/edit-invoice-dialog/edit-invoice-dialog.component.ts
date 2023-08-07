61ea90cc521255df4baaf635ae89a6fdb6ed20dd
Dirk Brandon Lapitan <dbslapitan@gmail.com>
2023-08-07 05:02:42 -0400

    Initial Structur of items

--- src/app/invoices-home/edit-invoice-dialog/edit-invoice-dialog.component.ts
+++ src/app/invoices-home/edit-invoice-dialog/edit-invoice-dialog.component.ts
@@ -2,7 +2,7 @@ import {Component, ElementRef, HostListener, Inject, OnInit} from '@angular/core
 import {Dialog, DIALOG_DATA} from "@angular/cdk/dialog";
 import {Invoice} from "../../models/invoice.model";
 import {Item} from "../../models/item.model";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {CDK_MENU} from "@angular/cdk/menu";
 import {CdkScrollable, ConnectedPosition, ScrollDispatcher} from "@angular/cdk/overlay";
 import {Observable} from "rxjs";
@@ -63,8 +63,27 @@ export class EditInvoiceDialogComponent implements OnInit{
       clientCountry: [this.invoice.clientAddress.country, Validators.required],
       invoiceDate: [this.invoice.createdAt, Validators.required],
       paymentTerm: [this.invoice.paymentTerms, Validators.required],
-      projectDescription: [this.invoice.description, Validators.required]
+      projectDescription: [this.invoice.description, Validators.required],
+      items: this.fb.array([])
     });
+    this.addItems();
+  }
+
+  get getItems(){
+    return this.editForm.controls['items'] as FormArray;
+  }
+
+  addItems(){
+
+    for(let item of this.items){
+      const itemForm = this.fb.group({
+        name: [item.name, Validators.required],
+        quantity: [item.quantity, Validators.required],
+        price: [item.price, Validators.required],
+        total: [item.total, Validators.required]
+      });
+      this.getItems.push(itemForm);
+    }
   }
 
   goBackClicked(){
@@ -91,4 +110,6 @@ export class EditInvoiceDialogComponent implements OnInit{
       flag.checked = this.menuIsOpen;
     }
   }
+
+  protected readonly FormGroup = FormGroup;
 }
